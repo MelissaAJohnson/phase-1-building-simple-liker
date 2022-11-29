acbe7d6a5ea6be7f15353ca0014879e2e57bb5c3
Melissa Johnson <missyj2007@gmail.com>
2022-11-28 19:18:18 -0600

    initial commit

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// Find the like icon
+const likeIcons = document.querySelectorAll('span.like-glyph');
 
+// Because the find returns an array object, iterate through the array
+for (let i = 0; i < likeIcons.length; i++) {
+  (function(){
+    // Listen for a click for each iteration
+    likeIcons[i].addEventListener("click", function() {
+      const myPromise = mimicServerCall()
+      const element = document.getElementById("modal")
+      myPromise.then(changeHeart(i))
+      myPromise.catch(function(error) {
+        element.classList.remove("hidden")
+        element.innerHTML = error
+        setTimeout(function() {
+          element.classList.add("hidden")
+        }, 3000)
+      })
+    })
+  })(i);
+}
 
-
+function changeHeart(i) {
+  if (likeIcons[i].classList.contains('activated-heart')) {
+    likeIcons[i].innerHTML = EMPTY_HEART;
+  } else {
+    likeIcons[i].innerHTML = FULL_HEART;
+  }
+  // Toggle the icon styling
+  likeIcons[i].classList.toggle('activated-heart')
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
